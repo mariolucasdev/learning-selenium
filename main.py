a128e68443e5df1f74a86193da05c48436a70b88
Mário Lucas <mariolucasdev@gmail.com>
2022-02-10 22:28:02 -0300

    create readme and reafactory main

--- main.py
+++ main.py
@@ -7,11 +7,14 @@ from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
 
+id_user_fild = "username"
+id_pswd_fild = "password"
+
 driver.get(host)
 driver.implicitly_wait(0.5)
 
-input_login = driver.find_element(By.ID, "username")
-input_password = driver.find_element(By.ID, "password")
+input_login = driver.find_element(By.ID, id_user_fild)
+input_password = driver.find_element(By.ID,id_pswd_fild)
 
 input_login.send_keys(user)
 input_password.send_keys(pswd,  Keys.ENTER)
---
+++ readme.md
@@ -0,0 +1,19 @@
+# Study Selenium
+
+### Auth in form web with selenium
+
+This is a simple script to make login in website.
+
+### Use
+
+  01 - Create a file constants.py in root directory
+  
+  02 - Create variables:
+    - host: url to form login
+    - user: user to login
+    - pswd: password to login
+  
+  03 - Set the fields id in variables id_user_field and id_pswd_field
+  
+  04 - Run code with
+   ```py main.py```
